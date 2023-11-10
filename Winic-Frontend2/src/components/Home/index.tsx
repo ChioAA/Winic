03b38d7f06d97258c4edb591ac0ad077da70a1df
Hectorc70 <hecg.dev@gmail.com>
2023-11-10 07:18:09 -0600

    sd

--- Winic-Frontend2/src/components/Home/index.tsx
+++ Winic-Frontend2/src/components/Home/index.tsx
@@ -2,11 +2,9 @@
 const Home = () => {
 
   return (
-    <div className="bg-globalWhite  flex flex-col sm:overflow-auto">
-      <div className="relative h-auto sm:h-3/4 sm:mt-5 flex w-full flex-row items-center justify-center">
-          <div className="md:overflow-auto h-full md:px-10 px-1 mt-3">
-          </div>
-      </div>
+    <div className="flex flex-row w-4/6">
+      X
+      
     </div>
 
   )
--- Winic-Frontend2/src/components/Layout/Layout.tsx
+++ Winic-Frontend2/src/components/Layout/Layout.tsx
@@ -5,11 +5,11 @@ const Layout = () => {
   const navigate  = useNavigate()
 
   return (
-    <div className="bg-globalWhite  flex flex-row sm:overflow-auto">
-      <div className="min-w-xs w-90 max-w-xs flex flex-col h-48 rounded-md shadow-md cursor-pointer m-5 hover:shadow-2xl " onClick={()=>navigate(routesNames.upload)}  >
-            <span className="bg-purple text-globalWhite text-center font-bold py-1 rounded-b-md w-full">Subir producto</span>
+    <div className="bg-globalWhite  flex flex-row w-full">
+      <div className="min-w-xs w-90 h-20 max-w-xs flex flex-col h-48 rounded-md shadow-md cursor-pointer m-5 hover:shadow-2xl " onClick={()=>navigate(routesNames.upload)}  >
+            <span className="bg-purple h-20 text-globalWhite text-center font-bold py-1 rounded-b-md w-full">Subir producto</span>
       </div>
-      <div className="min-w-xs w-90 max-w-xs flex flex-col h-48 rounded-md shadow-md cursor-pointer m-5 hover:shadow-2xl"  >
+      <div className="min-w-xs w-90 h-20 max-w-xs flex flex-col h-48 rounded-md shadow-md cursor-pointer m-5 hover:shadow-2xl"  >
             <span className="bg-purple text-globalWhite text-center font-bold py-1 rounded-b-md w-full">Ver productos</span>
       </div>
     </div>
